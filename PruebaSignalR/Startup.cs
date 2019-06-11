a092639859511ae4430162c8f107d45686b6d55d
NeXuZZ <NeXuZZ@outlook.com>
2019-06-11 10:15:52 -0300

    Documentacion

---
+++ PruebaSignalR/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(PruebaSignalR.Startup))]
+
+namespace PruebaSignalR
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR(); // Para obtener más información sobre cómo configurar la aplicación, visite https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
---
+++ PruebaSignalR/controller.js
@@ -0,0 +1,13 @@
+﻿var app = angular.module("myApp", []);
+
+app.controller("myCtrl", function ($scope) {
+    $scope.conn = $.connection.pruebaHub;   
+    $scope.conn.client.hello = function () {
+        alert("dddd");
+    };
+    $scope.probar = function () {
+        $.connection.hub.start().done(function () {
+            $scope.conn.server.hello();
+        });
+    }
+});
